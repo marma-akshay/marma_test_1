cd3f120a120ae1b7add3bd13ce06791b715589fd
Akshay <akshay@marma.ai>
2023-08-13 17:28:17 +0530

    adding dark theme for app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, createContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PublicLayout from './components/PublicLayout';
 import Layout from './components/Layout';
@@ -11,19 +11,25 @@ import Dashboard from './components/Dashboard';
 import Profile from './components/Profile';
 import './App.css';
 
+export const ThemeContext = createContext();
+
 function App() {
+  const [theme, setTheme] = useState('dark'); // Default theme is set to dark
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PublicLayout><LandingPage /></PublicLayout>} />
-        <Route path="/signin" element={<PublicLayout><SignIn /></PublicLayout>} />
-        <Route path="/signup" element={<PublicLayout><SignUp /></PublicLayout>} />
-        <Route path="/faq" element={<PublicLayout><FAQ /></PublicLayout>} />
-        <Route path="/aboutus" element={<PublicLayout><AboutUs /></PublicLayout>} />
-        <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
-        <Route path="/profile" element={<Layout><Profile /></Layout>} />
-      </Routes>
-    </Router>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PublicLayout><LandingPage /></PublicLayout>} />
+          <Route path="/signin" element={<PublicLayout><SignIn /></PublicLayout>} />
+          <Route path="/signup" element={<PublicLayout><SignUp /></PublicLayout>} />
+          <Route path="/faq" element={<PublicLayout><FAQ /></PublicLayout>} />
+          <Route path="/aboutus" element={<PublicLayout><AboutUs /></PublicLayout>} />
+          <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+          <Route path="/profile" element={<Layout><Profile /></Layout>} />
+        </Routes>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
--- frontend/src/components/PublicLayout.js
+++ frontend/src/components/PublicLayout.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './PublicLayout.css';
+import logo from '../assets/logo.png'; // Import your logo image
 
 const PublicLayout = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    // Toggle dark mode styles on the body element
+    document.body.classList.toggle('dark-mode');
+  };
+
   return (
-    <div className="public-layout">
+    <div className={`public-layout ${darkMode ? 'dark-mode' : ''}`}>
       <header className="header">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/faq">FAQ</Link>
-          <Link to="/signin">Sign In</Link>
-          <Link to="/signup">Sign Up</Link>
+        <nav className="nav">
+          <div className="nav-links">
+            <img src={logo} alt="Marma Logo" className="logo" />
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/faq">FAQ</Link>
+          </div>
+          <div className="nav-links">
+            <button className="button" onClick={toggleDarkMode}>
+              {darkMode ? 'Light Mode' : 'Dark Mode'}
+            </button>
+            <Link to="/signin">Sign In</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
         </nav>
       </header>
       <main className="main-content">
