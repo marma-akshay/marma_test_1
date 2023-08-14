d1ce9ead44b660d952836d94fcbf017628b265a8
Akshay <akshay@marma.ai>
2023-08-14 18:54:44 +0530

    ehanced landing page 1

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './LandingPage.css';
 
 function LandingPage() {
   return (
     <div className="landing-page">
-      <h1>Welcome to Marma!</h1>
-      <p>Explore, Learn, and Master Data Analytics.</p>
+      <div className="tunnel">
+        <h1>Welcome to Marma!</h1>
+        <p>Explore, Learn, and Master Data Analytics.</p>
+      </div>
       <div className="buttons">
-        <Link to="/signin">Sign In</Link>
-        <Link to="/signup">Sign Up</Link>
+        <Link to="/signup" className="button explore-button">Start Your Exploration</Link>
       </div>
     </div>
   );
