376a9c5a0d2a4601c5e2d0082240a361fc6f563d
Akshay <akshay@marma.ai>
2023-09-09 02:39:34 +0530

    adding line effect at bottom and increasing size

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -7,10 +7,23 @@ function LandingPage() {
   for (let i = 0; i < 200; i++) {
     const side = Math.floor(Math.random() * 4);
     const position = Math.random() * 100 + "vw";
+    const size = Math.random() * 1.3 + "em";
+
+    let character;
+    if (Math.random() < 0.8) {
+      // 80% chance to generate a number
+      character = String.fromCharCode(48 + Math.floor(Math.random() * 10));
+    } else {
+      // 20% chance to generate a special symbol
+      const specialSymbols = ['%', '#', '&', '@', '$','a','d','g','l'];
+      character = specialSymbols[Math.floor(Math.random() * specialSymbols.length)];
+    }
+
     const point = {
-      character: String.fromCharCode(33 + Math.floor(Math.random() * (126 - 33))),
+      character: character,
       side: side,
       position: position,
+      size: size,
       animationDelay: `${Math.random() * 5}s`
     };
     dataPoints.push(point);
@@ -19,19 +32,21 @@ function LandingPage() {
   return (
     <div className="landing-page">
       {dataPoints.map((point, index) => {
-        let style = {};
+        let style = {
+          fontSize: point.size,  // Applying random size to data points
+        };
         switch (point.side) {
           case 0:
-            style = { top: 0, left: point.position, animation: `moveToPointTop ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
+            style = { ...style, top: 0, left: point.position, animation: `moveToPointTop ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
             break;
           case 1:
-            style = { bottom: 0, left: point.position, animation: `moveToPointBottom ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
+            style = { ...style, bottom: 0, left: point.position, animation: `moveToPointBottom ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
             break;
           case 2:
-            style = { left: 0, top: point.position, animation: `moveToPointLeft ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
+            style = { ...style, left: 0, top: point.position, animation: `moveToPointLeft ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
             break;
           case 3:
-            style = { right: 0, top: point.position, animation: `moveToPointRight ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
+            style = { ...style, right: 0, top: point.position, animation: `moveToPointRight ${5 + Math.random() * 5}s infinite`, animationDelay: point.animationDelay };
             break;
           default:
             break;
@@ -51,7 +66,7 @@ function LandingPage() {
       </div>
       <div className="welcome-section">
         <h1>Welcome to Marma!</h1>
-        <p>Explore, Learn, and Master Data Analytics.</p>
+        <p>Where learning makes sense!.</p>
         <div className="buttons">
           <Link to="/signup" className="button">
             Explore
