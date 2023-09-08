ce5447ef0b0973657d06c5938f5642e638829792
Akshay <akshay@marma.ai>
2023-09-09 03:42:27 +0530

    sign up page better format

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './SignUp.css'; // Import the new CSS file
+import { useNavigate, Link } from 'react-router-dom';
+import './SignUp.css';
 
 function SignUp() {
   const [username, setUsername] = useState('');
@@ -9,9 +9,19 @@ function SignUp() {
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
+  const dataPoints = [];
+  for (let i = 0; i < 50; i++) {
+    const position = Math.random() * 100 + "vh";
+    const point = {
+      character: String.fromCharCode(33 + Math.floor(Math.random() * (126 - 33))),
+      position: position,
+      animationDelay: `${Math.random() * 5}s`
+    };
+    dataPoints.push(point);
+  }
+
   const handleSignUp = async (e) => {
     e.preventDefault();
-
     try {
       const response = await fetch('http://localhost:3001/auth/register', {
         method: 'POST',
@@ -35,14 +45,36 @@ function SignUp() {
 
   return (
     <div className="sign-up-container">
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSignUp}>
-        <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} required />
-        <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
-        <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
-        <button type="submit" className="sign-up-button">Sign Up</button>
-      </form>
-      {error && <p className="error-message">{error}</p>}
+      {dataPoints.map((point, index) => {
+        let style = {
+          top: point.position, 
+          left: '0', 
+          animation: `moveToPointTopSignUp ${5 + Math.random() * 5}s infinite`, 
+          animationDelay: point.animationDelay 
+        };
+        return (
+          <span key={index} className="data-point-signup" style={style}>{point.character}</span>
+        );
+      })}
+      
+      <div className="auth-options-signup">
+        <div className="auth-options-content-signup">
+          <Link to="/signin">Log In</Link>
+          <Link to="/AboutUs">About</Link>
+          <Link to="/FAQ">FAQ</Link>
+        </div>
+      </div>
+
+      <div className="sign-up-glass">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSignUp} className="sign-up-form">
+          <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} required />
+          <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+          <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+          <button type="submit" className="sign-up-button-signup">Sign Up</button>
+        </form>
+        {error && <p className="error-message-signup">{error}</p>}
+      </div>
     </div>
   );
 }
